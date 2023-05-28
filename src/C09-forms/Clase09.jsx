25ca2e5216f751e26136134b0946e939362cb0ef
paulagoldenhorn <paulagoldenhorn@gmail.com>
2023-05-28 13:33:15 -0300

    Ejercicio clase 09 modificado: optimizacion de rendimiento

--- src/C09-forms/Clase09.jsx
+++ src/C09-forms/Clase09.jsx
@@ -1,9 +1,49 @@
+import { useState } from 'react'
+import { generateId } from './idGenerator'
 import Formulario from './Formulario'
+import Lista from '../C05-estilos/Lista/Lista'
+import Item from '../C05-estilos/Item/Item'
+
+/*
+Responsabilidades:
+- Formulario muestra el formulario y renderiza los errores
+- Clase09 muestra Formulario y renderiza los resultados
+*/
 
 function Clase09() {
+  const [submittedValues, setSubmittedValues] = useState([])
+
+  function handleSubmit(values) {
+    setSubmittedValues([
+      ...submittedValues,
+      {
+        id: generateId(),
+        number: values.inputNumber,
+        name: values.inputName,
+      },
+    ])
+  }
+
   return (
     <>
-        <Formulario />
+      <h1>¡Nuestros números favoritos!</h1>
+      <Formulario onSubmit={handleSubmit} />
+
+      <br />
+
+      <h2>Números favoritos de los usuarios:</h2>
+      <Lista>
+        {submittedValues.map((value) => {
+          return (
+            <Item key={value.id}>
+              <p>Nombre: {value.name}</p>
+              <p style={{ color: 'lightGreen' }}>
+                Tu número favorito es: {value.number}
+              </p>
+            </Item>
+          )
+        })}
+      </Lista>
     </>
   )
 }
--- src/C09-forms/Formulario.jsx
+++ src/C09-forms/Formulario.jsx
@@ -1,18 +1,15 @@
 import { useState } from 'react'
-import Item from '../C05-estilos/Item/Item'
-import Lista from '../C05-estilos/Lista/Lista'
-import { generateId } from './idGenerator'
+import styles from '../C05-estilos/Item/Item.module.css'
 
-function Formulario() {
+function Formulario(props) {
   const [inputNumber, setInputNumber] = useState('')
   const [inputName, setInputName] = useState('')
 
   const [error, setError] = useState('')
-  const [submittedValues, setSubmittedValues] = useState([])
 
   function hasErrors() {
     if (!inputName) {
-      setError('Debe ingresar un nombre')
+      setError('Debes ingresar un nombre')
       return true
     } else if (!inputNumber || inputNumber < 0) {
       setError('Debes ingresar un número mayor que 0')
@@ -25,14 +22,7 @@ function Formulario() {
 
     if (hasErrors()) return
 
-    setSubmittedValues([
-      ...submittedValues,
-      {
-        id: generateId(),
-        number: inputNumber,
-        name: inputName,
-      },
-    ])
+    props.onSubmit({ inputName, inputNumber })
 
     setInputNumber('')
     setInputName('')
@@ -40,52 +30,35 @@ function Formulario() {
   }
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor='name'>Ingrese su nombre</label>
-        <br />
-        <input
-          placeholder='Nombre'
-          value={inputName}
-          name='name'
-          type='text'
-          onChange={(e) => setInputName(e.target.value)}
-        ></input>
-
-        <br />
-
-        <label htmlFor='number'>Ingrese su número favorito</label>
-        <br />
-        <input
-          placeholder='Número'
-          value={inputNumber}
-          name='number'
-          type='number'
-          onChange={(e) => setInputNumber(e.target.value)}
-        ></input>
-
-        <br />
-
-        <button type='submit'>Enviar</button>
-      </form>
-      {error ? <p style={{ color: 'red' }}>{error}</p> : undefined}
+    <form onSubmit={handleSubmit} className={styles.item}>
+      <label htmlFor='name'>Ingrese su nombre</label>
+      <br />
+      <input
+        placeholder='Nombre'
+        value={inputName}
+        name='name'
+        type='text'
+        onChange={(e) => setInputName(e.target.value)}
+      ></input>
 
       <br />
-      {/* Esto se renderiza cada vez que hay un cambio en 'submittedValues' :/ */}
-      <Lista>
-        {submittedValues.map(value => {
-          return (
-            <Item key={value.id}>
-                <p>Nombre: {value.name}</p>
-                <p style={{ color: 'lightGreen' }}>
-                  Tu número favorito es: {value.number}
-                </p>
-            </Item>
-          )
-        })}
-      </Lista>
 
-    </>
+      <label htmlFor='number'>Ingrese su número favorito</label>
+      <br />
+      <input
+        placeholder='Número'
+        value={inputNumber}
+        name='number'
+        type='number'
+        onChange={(e) => setInputNumber(e.target.value)}
+      ></input>
+
+      <br />
+
+      <button type='submit'>Enviar</button>
+      
+      {error ? <p style={{ color: 'red' }}>{error}</p> : undefined}
+    </form>
   )
 }
 
